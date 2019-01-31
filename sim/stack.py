6287a6b0faaa04571a7876dda7a7ebb9ad1b1179
Thomas Aarholt <thomasaarholt@gmail.com>
2019-01-31 17:31:44 +0100

    upgrades

--- sim/stack.py
+++ sim/stack.py
@@ -1,6 +1,7 @@
 import pathlib
 import hyperspy.api as hs
 from sim.fileio import readMRC
+from sim.voronoi import integrate
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -21,6 +22,17 @@ def stack_and_save():
     for p in pathlib.Path('./prism').iterdir():
         if "_Prismatic" in p.name:
             files.append(p.name)
+
+    headers = list(set([f.split('_FP')[0] for f in files]))
+
+    group_of_files = []
+    for header in headers:
+        files = []
+        for p in pathlib.Path('./prism').iterdir():
+            if header in p.name and not p.is_dir() and p.suffix == ".mrc":
+                files.append(p.name)
+        files = sorted(files)
+        group_of_files.append(files)
 
     def save(files, name):
         def read(filenames):
@@ -51,8 +63,12 @@ def stack_and_save():
         s.axes_manager[3].units = 'Å'
         print('Begun saving!')
 
+        haadf = s.inav[40.:].sum()
         fig, ax = plt.subplots(dpi=200)
-        im = ax.imshow(s.inav[40.:].sum().data)
+        im = ax.imshow(haadf.data)
+
+        fig2, ax = plt.subplots(dpi=200)
+        im2 = ax.imshow(integrate(haadf).data)
 
         def saveimg(filepath, fig=None):
             '''Save the current image with no whitespace
@@ -73,20 +89,21 @@ def stack_and_save():
 
 
 
-        s.save("hyperspy/" + name, overwrite=True)
-        saveimg("hyperspy/" + name + "_HAADF_sum.png")
+        s.save("hyperspy/" + name + ".hspy", overwrite=True)
+        saveimg("hyperspy/" + name + "_HAADF_sum.png", fig=fig)
+        saveimg("hyperspy/" + name + "_voronoi.png", fig=fig2)
 
 
 
-    for model in model_files:
-        load_files = sorted(["prism/" + f for f in files if f.startswith(model) and "thermal" not in f and f.endswith('.mrc')])
+    for files in group_of_files:
+        load_files = sorted(["prism/" + f for f in files if "nothermal" not in f])
         if load_files:
-            name = load_files[0].split("_FP")[0]
+            name = load_files[0].split("prism/")[1].split("_FP")[0]
             print('Stacking {}'.format(name))
             save(load_files, name)
 
-        load_files = sorted(["prism/" + f for f in files if f.startswith(model) and "thermal" in f and f.endswith('.mrc')])
+        load_files = sorted(["prism/" + f for f in files if "nothermal" in f])
         if load_files:
-            name = load_files[0].split("_FP")[0]
+            name = load_files[0].split("prism/")[1].split("_FP")[0]
             print('Stacking {}'.format(name))
-            save(load_files, name + "_thermal")
+            save(load_files, name + "_nothermal")
