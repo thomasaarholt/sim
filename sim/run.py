01bf59be23487a3f38765d0d8bc4fc0fc6d192a3
Thomas Aarholt <thomasaarholt@gmail.com>
2019-02-27 13:36:22 +0100

    removed lable

--- sim/run.py
+++ sim/run.py
@@ -11,8 +11,8 @@ for p in pathlib.Path('./Models').iterdir():
 files = sorted(files)#, key=key)
 
 for filename in files:
-    prismatic(filename, label="_slice1.6", limits = ({0}, {1}), PRISM=True, savepath='prism', thermal_effects=False, total_FP=1, sliceThickness=1.6218179)
-    prismatic(filename, label="_slice1.6", limits = ({0}, {1}), PRISM=True, savepath='prism', thermal_effects=True, total_FP=20, sliceThickness=1.6218179)
+    prismatic(filename, label="", limits = ({0}, {1}), PRISM=True, savepath='prism', thermal_effects=False, total_FP=1, sliceThickness=1.6218179)
+    prismatic(filename, label="", limits = ({0}, {1}), PRISM=True, savepath='prism', thermal_effects=True, total_FP=20, sliceThickness=1.6218179)
 
 from sim import stack_and_save
 stack_and_save()
