4090810b9c8e71eb4e31f65abb91e52cf34fa0f4
Thomas Aarholt <thomasaarholt@gmail.com>
2019-03-05 14:32:34 +0100

    made func

--- sim/test.py
+++ sim/test.py
@@ -1,19 +1,22 @@
-from pyprismatic.params import Metadata
-import os
-with open('temp.XYZ', 'w') as fid:
-    fid.write("one unit cell of 100 silicon\n\
-  5.43    5.43    5.43\n\
-14  0.0000  0.0000  0.0000  1.0  0.076\n\
-14  2.7150  2.7150  0.0000  1.0  0.076\n\
-14  1.3575  4.0725  1.3575  1.0  0.076\n\
-14  4.0725  1.3575  1.3575  1.0  0.076\n\
-14  2.7150  0.0000  2.7150  1.0  0.076\n\
-14  0.0000  2.7150  2.7150  1.0  0.076\n\
-14  1.3575  1.3575  4.0725  1.0  0.076\n\
-14  4.0725  4.0725  4.0725  1.0  0.076\n\
--1")
-meta = Metadata(filenameAtoms='temp.XYZ', filenameOutput='output.mrc')
-meta.algorithm = 'multislice'
-meta.alsoDoCPUWork = False
-os.remove("temp.XYZ")
-os.remove("output.mrc")
+def test():
+    import os
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    from pyprismatic.params import Metadata
+
+    with open('temp.XYZ', 'w') as fid:
+        fid.write("one unit cell of 100 silicon\n\
+    5.43    5.43    5.43\n\
+    14  0.0000  0.0000  0.0000  1.0  0.076\n\
+    14  2.7150  2.7150  0.0000  1.0  0.076\n\
+    14  1.3575  4.0725  1.3575  1.0  0.076\n\
+    14  4.0725  1.3575  1.3575  1.0  0.076\n\
+    14  2.7150  0.0000  2.7150  1.0  0.076\n\
+    14  0.0000  2.7150  2.7150  1.0  0.076\n\
+    14  1.3575  1.3575  4.0725  1.0  0.076\n\
+    14  4.0725  4.0725  4.0725  1.0  0.076\n\
+    -1")
+    meta = Metadata(filenameAtoms='temp.XYZ', filenameOutput='output.mrc')
+    meta.algorithm = 'multislice'
+    meta.alsoDoCPUWork = False
+    os.remove("temp.XYZ")
+    os.remove("output.mrc")
