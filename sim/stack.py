91ff9f623f9d7165ee2f9f780275ca4f114e68a5
Thomas Aarholt <thomasaarholt@gmail.com>
2019-03-11 18:30:16 +0100

    add colorbar

--- sim/stack.py
+++ sim/stack.py
@@ -5,6 +5,7 @@ from sim.stats import get_atom_indices, plot_standard_error_with_phonons
 from sim.defocus import get_series_defocus_and_weight
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import glob
 from pathlib import Path
 from tqdm.auto import tqdm
@@ -122,11 +123,13 @@ def save2(s, haadf_FP_depth_series, corename, depths, save_hspy=True, add_atom_p
 
         fig, ax = plt.subplots(dpi=200)
         im = ax.imshow(haadf.data)
+        colorbar(im)
         saveimg("hyperspy/" + name + "_HAADF_sum.png", fig=fig)
 
         I, IM, PM = integrate(haadf, add_atom_positions)
         fig2, ax = plt.subplots(dpi=200)
         im2 = ax.imshow(IM.data)
+        colorbar(im2)
         saveimg("hyperspy/" + name + "_voronoi.png", fig=fig2)
 
         if len(haadf_FP_series.axes_manager.navigation_axes):
@@ -200,7 +203,7 @@ def error(I, name):
     fig.savefig("hyperspy/" + name + "_error.png", dpi=200)
 
 
-def saveimg(filepath, fig=None):
+def saveimg(filepath="image.png", fig=None):
     '''Save the current image with no whitespace
     Example filepath: "myfig.png" or r"C:\myfig.pdf" 
     Based on answers from https://stackoverflow.com/questions/11837979/
@@ -208,14 +211,27 @@ def saveimg(filepath, fig=None):
     import matplotlib.pyplot as plt
     if not fig:
         fig = plt.gcf()
-
-    plt.subplots_adjust(0, 0, 1, 1, 0, 0)
+        
+    plt.subplots_adjust(0,0,1,1,0,0)
     for ax in fig.axes:
+        if ax.aname == 'colorbar':
+            continue
         ax.axis('off')
-        ax.margins(0, 0)
+        ax.margins(0,0)
         ax.xaxis.set_major_locator(plt.NullLocator())
         ax.yaxis.set_major_locator(plt.NullLocator())
-    fig.savefig(filepath, pad_inches=0, bbox_inches='tight')
+    
+    fig.savefig(filepath, pad_inches = 0, bbox_inches='tight')
+
+
+def colorbar(mappable):
+    "mappable is img = plt.imshow()"
+    ax = mappable.axes
+    fig = ax.figure
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cax.aname = 'colorbar'
+    return fig.colorbar(mappable, cax=cax)
 
 
 def get_depth_and_defocus_lists(names):
