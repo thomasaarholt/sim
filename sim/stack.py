fdc42b258ed1453358566dc61708e3acfea4792c
Thomas Aarholt <thomasaarholt@gmail.com>
2019-03-18 15:12:34 +0100

    wip

--- sim/stack.py
+++ sim/stack.py
@@ -108,7 +108,7 @@ def stack_and_save(simulation_folder='prism', add_atom_positions=False, save_hsp
 
         save3(s, corename + "_d{:02}".format(depth), add_atom_positions)
 
-def save3(s, name, add_atom_positions):
+def save3(s, name, add_atom_positions=False):
     tqdm.write("Saving {}".format(name))
     plt.close('all')
     Path('hyperspy/').mkdir(parents=True, exist_ok=True)
