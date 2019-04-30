031b5327960e613ed3997dcf13aed5fcbe3f3ea2
Thomas Aarholt <thomasaarholt@gmail.com>
2019-04-30 15:46:02 +0200

    fix

--- sim/stack.py
+++ sim/stack.py
@@ -188,12 +188,12 @@ def save3(s, name):
     s.save("hyperspy/" + name + ".hspy", overwrite=True)
     tqdm.write('...ok, took {} seconds'.format(time() - t))
 
-    if len(haadf_FP.axes_manager.navigation_axes):
-        tqdm.write('\t' + 'Calculating error', end="")
-        t = time()
-        I, IM, PM = integrate(haadf, False, True)
-        error(I, name)
-        tqdm.write('...ok, took {} seconds'.format(time() - t))
+    # if len(haadf_FP.axes_manager.navigation_axes):
+    #     tqdm.write('\t' + 'Calculating error', end="")
+    #     t = time()
+    #     I, IM, PM = integrate(haadf, False, True)
+    #     error(I, name)
+    #     tqdm.write('...ok, took {} seconds'.format(time() - t))
 
 
 def save2(
@@ -218,9 +218,9 @@ def save2(
         colorbar(im2)
         saveimg("hyperspy/" + name + "_voronoi.png", fig=fig2)
 
-        # if len(haadf_FP_series.axes_manager.navigation_axes):
-        #     I, IM, PM = integrate(haadf_FP_series, add_atom_positions)
-        #     error(I, name)
+        if len(haadf_FP_series.axes_manager.navigation_axes):
+            I, IM, PM = integrate(haadf_FP_series, add_atom_positions)
+            error(I, name)
     if save_hspy:
         s.save("hyperspy/" + corename + ".hspy", overwrite=True)
 
